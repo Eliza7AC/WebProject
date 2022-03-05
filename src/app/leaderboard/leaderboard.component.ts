fdcb566ed42bfe6a104add5481be3930ea51d4ce
Samuel <samuel.gras@etu.univ-amu.fr>
2022-03-05 01:09:22 +0100

    functional leaderboard + chat history

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,15 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Score } from '../models/score.model';
+import { Subscription } from "rxjs";
+import { HttpService } from "../services/http.service";
+
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.css']
 })
-export class LeaderboardComponent implements OnInit {
+export class LeaderboardComponent implements OnInit, OnDestroy {
+  scoreboard: Score[] = [];
+  scoreSubscription!: Subscription;
+  testInt: number = 0;
 
-  constructor() { }
+  constructor(private http: HttpService) { }
 
   ngOnInit(): void {
+    this.scoreSubscription = this.http.getScores().subscribe({
+      next: scores  => {
+        this.scoreboard = scores;
+      },
+      error: err => {
+        console.log('erreur ', err)
+      },
+      complete: () => {
+        this.scoreboard.sort((a, b) => (a.points < b.points)? 1 : -1);
+      }
+    });
   }
 
+  onSuppress(username: string) {
+    if (confirm('Voulez-vous vraiment supprimer le score de ' + username + ' ?')) {
+      this.suppressScore(username);
+    }
+  }
+
+  suppressScore(username: string) {
+    this.http.suppScore(username).subscribe( (result) => {
+      if (result.status === 200) {
+        this.ngOnInit();
+      } else {
+        alert('Ce score n\'existe pas !');
+      }
+    })
+  }
+
+  // Pour tester
+  onTest() {
+    this.http.createScore(new Score(this.makeUsername(10), Math.floor(Math.random() * 20))).subscribe({
+      next: response => {
+        if (response && response.username) {
+          alert('Score créé');
+        } else {
+          alert('Le score pour ce joueur existe déjà !')
+        }
+      },
+      error: err => {
+        console.log('erreur ', err)
+      },
+      complete: () => {
+        this.ngOnInit();
+      }
+    })
+  }
+
+  // Pour tester
+  makeUsername(length: number): string {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
+  }
+
+  // Pour tester
+  increment(score: Score): void {
+    score.points++;
+    this.http.modifyScore(score).subscribe({
+      next: response => response,
+      error: err => {
+        console.log('erreur ', err)
+      },
+      complete: () => {
+        setTimeout(this.ngOnInit, 2000);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.scoreSubscription?.unsubscribe();
+  }
 }
